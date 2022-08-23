1149a15449ab34d36217cfb1d9da92c6a90b5f32
cwhinfrey <cwhinfrey@gmail.com>
2022-08-22 18:41:52 -0700

    Add claimMultiple test

--- test/index.ts
+++ test/index.ts
@@ -81,6 +81,62 @@ describe("MerkleRewards", function () {
     expect(claimer2Balance.toString()).to.eq("3");
   });
 
+  it("Should execute a multiclaim", async function () {
+    const [, claimer1, claimer2] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("MockToken");
+    const token = await Token.deploy();
+    await token.deployed();
+
+    const MerkleRewards = await ethers.getContractFactory("MerkleRewards");
+    const merkleRewards = await MerkleRewards.deploy(token.address);
+    await merkleRewards.deployed();
+
+    await token.approve(merkleRewards.address, MAX_UINT);
+
+    const amount1 = BigNumber.from(5);
+    const leaf1 = solidityKeccak256(
+      ["bytes32", "address", "uint256"],
+      [LEAF_SALT, claimer1.address, amount1]
+    );
+
+    const amount2 = BigNumber.from(3);
+    const leaf2 = solidityKeccak256(
+      ["bytes32", "address", "uint256"],
+      [LEAF_SALT, claimer2.address, amount2]
+    );
+
+    const totalAmount = amount1.add(amount2);
+
+    const sortedLeaves = [leaf1, leaf2].sort();
+    const merkleRoot = solidityKeccak256(["bytes32", "bytes32"], sortedLeaves);
+    await merkleRewards.setMerkleRoot(merkleRoot, totalAmount);
+
+    if (sortedLeaves[0] === leaf1) {
+      await merkleRewards.claimMultiple(
+        [claimer1.address, claimer2.address],
+        [amount1, amount2],
+        [],
+        [true]
+      );
+    } else {
+      await merkleRewards.claimMultiple(
+        [claimer2.address, claimer1.address],
+        [amount2, amount1],
+        [],
+        [true]
+      );
+    }
+
+    const merkleRewardsBalance = await token.balanceOf(merkleRewards.address);
+    const claimer1Balance = await token.balanceOf(claimer1.address);
+    const claimer2Balance = await token.balanceOf(claimer2.address);
+
+    expect(merkleRewardsBalance.toString()).to.eq("0");
+    expect(claimer1Balance.toString()).to.eq("5");
+    expect(claimer2Balance.toString()).to.eq("3");
+  });
+
   it("Should revert if funds are claimed", async function () {
     const [, claimer1, claimer2] = await ethers.getSigners();
 
